49bf1d269573c492f549d6f2053dd3ed7eec975d
Maximiliano Merlo <carlos.maximiliano.merlo@gmail.com>
2023-11-16 22:19:18 -0300

    modal agregado con estilos y js, formspree integrado al formulario

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//funcion para que salga una ventana emergente(pop-up/modal) pidiendo verificar la edad del usuario apenas se carga la página
+function verifyAge(){
+    let userBirthYear = document.getElementById("age").value
 
+    const currentDate = new Date()
+    const currentYear = currentDate.getFullYear()
+
+    const  userAge= currentYear - userBirthYear
+
+    if (userAge >= 18) {
+        document.getElementById("ageVerification").style.display= "none";
+    }else{
+        document.getElementById("ageError").style.display= "block"
+    }   
+}
+
+window.onload = () => {
+    document.getElementById("ageVerification").style.display= "flex";
+}
